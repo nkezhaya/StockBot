fde5692662cd36aeee056894e53d1d70ecb51f9c
Nick Kezhaya <nick@whitepaperclip.com>
2009-12-28 19:45:09 -0600

    added weather module

---
+++ modules/w.rb
@@ -0,0 +1,15 @@
+
+def w(*args)
+  api = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='
+  zip = args[0].first
+
+  xml = RestClient.get(api + zip)
+  doc = Hpricot::XML(xml)
+  res = []
+
+  [ 'full', 'temperature_string', 'weather', 'local_time' ].each do |var|
+    res << doc.at(var).inner_html
+  end
+
+  say_to_chan res.join(' - ')
+end
