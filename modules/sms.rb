3889bd985a0559aee70c269132b81f8cd50194fa
Nick Kezhaya <nick@whitepaperclip.com>
2010-04-11 16:24:37 -0500

    DRY

--- modules/sms.rb
+++ modules/sms.rb
@@ -1,10 +1,10 @@
 def sms(*args)
   from  = '#stock@irc.freenode.net'
-  to    = args[0].flatten[0]
+  to    = args[0].flatten[0].downcase
   body  = args[0].flatten[1]
 
-  if $users[args[0].flatten[0].downcase] and $users[args[0].flatten[0].downcase][:sms]
-    to  = $users[args[0].flatten[0].downcase][:sms]
+  if $users[to] and $users[to][:sms]
+    to  = $users[to][:sms]
   end
 
   Pony.mail(:to => "#{to.to_i}@txt.att.net", :from => from, :body => body)
