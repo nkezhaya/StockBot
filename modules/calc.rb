2ae5703feff11a7cd2a3fb8dc223ced4217d0413
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-04-15 10:39:30 -0500

    Calculator

---
+++ modules/calc.rb
@@ -0,0 +1,15 @@
+def calc(*args)
+  arg = args[0].flatten
+  snd = arg.pop
+  url = "http://www.google.com/search?q=#{arg.join}"
+  dat = RestClient.get(url)
+  doc = Hpricot(dat.to_s)
+  r1  = doc.search("//h2[@class='r']")
+  r2  = r1.search("b").inner_html
+
+  unless r2.empty?
+    say_to_chan "#{snd}: #{r2}"
+  else
+    say_to_chan 'No results.'
+  end
+end
