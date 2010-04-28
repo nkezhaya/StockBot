a322cb5a96fde7a840279598592540ae493dcc90
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-04-28 02:50:19 -0500

    Made a new decide and 8ball

--- modules/decide.rb
+++ modules/decide.rb
@@ -1,8 +1,5 @@
 def decide(*args)
-  votes, yes = 10_000, 0
-  votes.times { yes += 1 if rand(2) == 1 }
-  result = (votes - yes > (votes / 2)) ? 'Yes' : 'No'
   sender = args[0].pop
 
-  say_to_chan "#{args.flatten.join(' ')} | Result: #{result}"
+  say_to_chan "#{args.flatten.sort_by { rand }.first}"
 end
