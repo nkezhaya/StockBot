650a5d5b11708722d833ed80796814db1ca8955b
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-03 18:24:46 -0500

    sanitized

--- modules/fml.rb
+++ modules/fml.rb
@@ -3,8 +3,13 @@ def fml(*args)
   doc = Hpricot(dat.to_s)
 
   post = doc.search("#wrapper").search(".post").first.search('p').first.inner_html
+  post.html_to_ascii!
   post.strip_html!
   post.gsub! /\s+/, ' '
 
-  say_to_chan post
+  post = Sanitize.clean(post)
+
+  post.scan(/.{1.350}/).each do |line|
+    say_to_chan line
+  end
 end
