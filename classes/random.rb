d03a851ad898b4ba76c6550f7745fd922de760aa
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-08 02:49:20 -0500

    max, not options

--- classes/random.rb
+++ classes/random.rb
@@ -1,6 +1,6 @@
 class Random
   def self.number(max = 10)
-    doc = RestClient.get "http://www.random.org/integers/?num=1&min=1&max=#{options.length}&col=1&base=10"
+    doc = RestClient.get "http://www.random.org/integers/?num=1&min=1&max=#{max}&col=1&base=10"
     dat = Hpricot(doc.to_s)
 
     dat.search("//pre[@class='data']").inner_html.strip.to_i
