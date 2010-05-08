640b1e808caadd2f3c84588ca9e12f1dcc212dd8
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-08 02:38:48 -0500

    True randomness

--- modules/decide.rb
+++ modules/decide.rb
@@ -1,5 +1,11 @@
 def decide(*args)
   sender = args[0].pop
+  options = args.flatten.sort_by { rand }
 
-  say_to_chan "#{args.flatten.sort_by { rand }.first}"
+  doc = RestClient.get "http://www.random.org/integers/?num=1&min=1&max=#{options.length}&col=1&base=10"
+  dat = Hpricot(doc.to_s)
+
+  result = options[dat.search("//pre[@class='data']").inner_html.strip.to_i - 1]
+
+  say_to_chan "#{result}"
 end
