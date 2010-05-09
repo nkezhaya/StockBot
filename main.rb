a6b69e80718f1d52abc5ae819edd4314f3953cc7
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-09 00:29:19 -0500

    multi-word decide

--- main.rb
+++ main.rb
@@ -2,5 +2,5 @@
 
 require 'config'
 
-bot = StockBot.new("irc.freenode.net", 6667, 'stock')
+bot = StockBot.new('irc.freenode.net', 6667, 'stock')
 bot.run
--- modules/decide.rb
+++ modules/decide.rb
@@ -1,7 +1,12 @@
 def decide(*args)
   sender = args[0].pop
-  options = args.flatten.sort_by { rand }
+
+  options = if args.join.include? ','
+              args.flatten.join(' ').split(',').sort_by { rand }
+            else
+              args.flatten.sort_by { rand }
+            end
 
   result = options[Random.number(options.length) - 1]
-  say_to_chan "#{result}"
+  say_to_chan "#{result.strip}"
 end
