e87f38305fcfdf5c09990f7e67dd28844a6685b0
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-10 01:30:26 -0500

    Timeout and sanitization for .title

--- modules/title.rb
+++ modules/title.rb
@@ -1,8 +1,15 @@
 def title(*args)
-  dat = RestClient.get(args.flatten.first.strip)
-  doc = Hpricot(dat.to_s)
+  begin
+    Timeout.timeout 3 do
+      dat = RestClient.get(args.flatten.first.strip)
+      doc = Hpricot(dat.to_s)
 
-  title = doc.search("title").inner_html.gsub(/(\n|\t)/, ' ').strip.gsub(/\s+/, ' ')
+      title = doc.search("title").inner_html.gsub(/(\n|\t)/, ' ').strip.gsub(/\s+/, ' ')
+      title = Sanitize.clean(title)
 
-  say_to_chan "URL Title: #{title}" unless title == ''
+      say_to_chan "URL Title: #{title}" unless title == ''
+    end
+  rescue Timeout::Error
+    say_to_chan 'Execution timeout.'
+  end
 end
