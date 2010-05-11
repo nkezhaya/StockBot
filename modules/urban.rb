b76fd80c7604ec0b677f643cd70527b8ee1d1235
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-10 20:20:08 -0500

    urban, proper

--- modules/urban.rb
+++ modules/urban.rb
@@ -1,33 +1,20 @@
 def urban(*args)
   sender  = args[0].pop
   term    = args.flatten.to_s rescue nil
+  html    = if term
+              RestClient.get("http://www.urbandictionary.com/define.php?term=#{CGI.escape(term)}")
+            else
+              RestClient.get('http://www.urbandictionary.com/random.php')
+            end
 
-  if term
-    html = RestClient.get("http://www.urbandictionary.com/define.php?term=#{CGI.escape(term)}")
-    doc = Hpricot(html.to_s)
+  begin
+    doc        = Hpricot(html.to_s)
+    definition = doc.at("meta[@name='Description']")['content'].split(' - ')
+    definition = definition.first + " - " + definition.last
 
-    begin
-      definition = doc.at("meta[@name='Description']")['content']
-      definition = definition.split(' - ')
-      definition = definition.first + " - " + definition.last
-
-      say_to_chan definition
-    rescue
-      say_to_chan 'Definition not found.'
-    end
-  else
-    html = RestClient.get("http://www.urbandictionary.com/random.php")
-    doc = Hpricot(html.to_s)
-    
-    begin
-      definition = doc.at("meta[@name='Description']")['content']
-      definition = definition.split(' - ')
-      definition = definition.first + " - " + definition.last
-
-      say_to_chan definition
-    rescue
-      say_to_chan 'Urban Dictionary is down.'
-    end
+    say_to_chan definition
+  rescue
+    say_to_chan 'Definition not found.'
   end
 end
 
