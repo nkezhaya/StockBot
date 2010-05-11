0aebf95e553ae15cf694c58afaba778a6305cf12
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-05-10 22:59:00 -0500

    fixed code

--- modules/solve.rb
+++ modules/solve.rb
@@ -1,18 +1,17 @@
 def solve(*args)
   sender  = args[0].pop
   term    = args.flatten.to_s rescue nil
-  html    = if term
-              RestClient.get("http://www.wolframalpha.com/input/?i=#{CGI.escape(term)}")
-            else
-              say to chan 'No equation set.'
-            end
 
-  begin
-    solved = html.to_s.scan(/"stringified": *"([^"]*)"/)
-
-    say_to_chan solved.flatten.first + ", " + solved.flatten.last
-  rescue
-    say_to_chan 'Unable to solve equation.'
+  if term
+    html = RestClient.get("http://www.wolframalpha.com/input/?i=#{CGI.escape(term)}")
+    begin
+      solved = html.to_s.scan(/"stringified": *"([^"]*)"/)
+      say_to_chan solved.flatten.first + ", " + solved.flatten.last
+    rescue
+      say_to_chan 'Unable to solve equation.'
+    end
+  else
+    say_to_chan 'No equation set.'
   end
 end
 
