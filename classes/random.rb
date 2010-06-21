4ca8519beadc6037460171a63bf373ac68ce8d3c
Nick Kezhaya <zarathu@whitepaperclip.whitepaperclip.com>
2010-06-21 16:03:46 -0500

    stupid naming conventions...

--- classes/random.rb
+++ classes/random.rb
@@ -1,8 +1,8 @@
 class Random
   def self.number(max = 10)
-    doc = RestClient.get "http://www.random.org/integers/?num=1&min=1&max=#{max}&col=1&base=10"
-    dat = Hpricot(doc.to_s)
+    dat = RestClient.get "http://www.random.org/integers/?num=1&min=1&max=#{max}&col=1&base=10"
+    doc = Hpricot(dat.to_s)
 
-    dat.search("//pre[@class='data']").inner_html.strip.to_i
+    doc.search("//pre[@class='data']").inner_html.strip.to_i
   end
 end
