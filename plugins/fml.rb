54774f404845ad5a50fbd9cd47dbcceb26618c53
User <nkezhaya@sls-ac6p12.sea2.superbservers.com>
2010-07-08 12:31:40 -0700

    api

--- plugins/fml.rb
+++ plugins/fml.rb
@@ -1,13 +1,12 @@
 module Plugins
-  API_KEY = '4be9c43fc03fe'
-  LANG    = 'en'
-  
   def fml(*args)
+    api_key = '4be9c43fc03fe'
+    lang    = 'en'
     sender  = args[0].pop
     term    = args.flatten.to_s rescue nil
 
     begin
-      xml = RestClient.get("http://api.betacie.com/view/random?key=#{API_KEY}&language=#{LANG}")
+      xml = RestClient.get("http://api.betacie.com/view/random?key=#{api_key}&language=#{lang}")
       doc  = Hpricot(xml.to_s)
       id = doc.search("//item").attributes["id"]
       fml = doc.search("//item").search("//text").first.inner_html
