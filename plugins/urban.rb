76bd54f218897e2665f8bd6581d6a564079508e8
fgoodman <frank+gh@frankgoodman.org>
2010-09-26 11:50:09 -0700

    removed the repetition feature

--- plugins/urban.rb
+++ plugins/urban.rb
@@ -2,16 +2,11 @@ module Plugins
   def urban(*args)
     sender  = args[0].pop
     term    = args.flatten.to_s rescue nil
-    html    = if term
-                RestClient.get("http://www.urbandictionary.com/define.php?term=#{CGI.escape(term)}")
-              else
-                RestClient.get('http://www.urbandictionary.com/random.php')
-              end
+    html    = RestClient.get("http://www.urbandictionary.com/define.php?term=#{CGI.escape(term)}")
 
     begin
       doc        = Hpricot(html.to_s)
       definition = doc.at("meta[@name='Description']")['content'].split(' - ')
-      definition = definition.first + " - " + definition.last
 
       say_to_chan definition
     rescue
